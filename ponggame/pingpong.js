34b9e1a32b1651b9dfd05bc0773b80aa3f48a1c0
KaiserJunior <67080078+KaiserJunior@users.noreply.github.com>
2020-09-07 10:32:57 +1200

    var for ball and user paddle done

--- ponggame/pingpong.js
+++ ponggame/pingpong.js
@@ -1,3 +1,21 @@
 const canvas = document.getElementById("pong");
 
-const ctx = canvas.getContext('2d';)
+const ctx = canvas.getContext('2d');
+
+const ball = {
+    x : canvas.width/2,
+    y : canvas.height/2,
+    radius : 10,
+    velocityX : 5,
+    velocityY : 5,
+    speed : 10,
+    color : "WHITE"
+}
+
+const user = {
+    x : 0, //left side of canvas
+    y : (canvas.height -100)/2, //-100 the height of paddle
+    width : 10,
+    height : 100,
+    color : "WHITE"
+}
