82f5463c0700aff47f55b2ac16c9aaa8af2b92c7
KaiserJunior <67080078+KaiserJunior@users.noreply.github.com>
2020-09-08 12:21:54 +1200

    drawn net

--- ponggame/pingpong.js
+++ ponggame/pingpong.js
@@ -28,3 +28,45 @@ const com = {
     score : 0,
     color : "WHITE"
 }
+
+const net = {
+    x : (canvas.width -2)/2,
+    y : 0,
+    height : 10,
+    width : 2,
+    color : "WHITE"
+}
+
+function drawRect(x,y,w,h, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,w,h);
+}
+
+function drawArc(x,y,r, color){
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x,y,r,0,Math.PI*2, true);
+    ctx.closePath();
+    ctx.fill();
+}
+
+canvas.addEventListener('mousemovement', getMousePos);
+
+function getMousePos(evt){
+    let rect = canvas.getBoundingClientRect();
+    
+    user.y = evt.clientY - rect.top - user.height/2;
+}
+
+function resetball(){
+    ball.x = canvas.width/2;
+    ball.y = canvas.height/2;
+    ball.velocityX =-ball.velocityx;
+    ball.speed = 10;
+}
+
+function drawNet(){
+    for(let i = 0; i <= canvas.height; i+=15){
+        drawRect(net.x, net.y + i, net.width, net.height, net.color);
+    }
+}
